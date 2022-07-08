d7a1b494f8c600d926d8cb37a930cee5685a7f04
Shivalika19 <103326946+Shivalika19@users.noreply.github.com>
2022-07-08 14:16:12 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+import Voting from "./voting";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Voting />
+    </div>
+  );
+}
